46dc4a2f1dde3b7ddb708bd8f7cbaaccefe93f18
Emmanuel <emmanuelfernandez00@gmail.com>
2025-06-01 09:40:19 +0000

    testing 27b model

--- rp_handler.py
+++ rp_handler.py
@@ -144,11 +144,11 @@ def load_model():
     # llm = LLM(model="google/gemma-3-12b-it")
     
     llm = LLM(
-        model="ISTA-DASLab/gemma-3-4b-it-GPTQ-4b-128g",
+        model="ISTA-DASLab/gemma-3-27b-it-GPTQ-4b-128g",
         # dtype="auto",
         # quantization="bitsandbytes",
         # load_format="bitsandbytes",
-        # max_model_len=8046
+        max_model_len=8046
     )
 
     sampling_params = SamplingParams(temperature=0, max_tokens=10)
